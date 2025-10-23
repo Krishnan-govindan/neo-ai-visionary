16d70d9ddb6cb997a57dfdd122fdea8db222019e
Krishnan-govindan <krishnan.meetup@gmail.com>
2025-10-23 14:16:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/neo-ai-visionary/", // <-- your repo name
+  base: "/", // <-- your repo name
   server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
